58a711dda39096f29c330385315b924c24fcce94
Aniket Raut <rautaniket640@gmail.com>
2024-04-10 17:32:39 +0530

    some changes video 23

---
+++ src/components/Button.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function Button({
+  children,
+  type = 'button',
+  bgColor = 'bg-blue-600',
+  textColor = 'text-white',
+  className = '',
+  ...props
+}) {
+  return (
+    <button
+      type={type} // Consider setting the type explicitly
+      className={`
+        px-4 py-2 rounded-lg
+        ${bgColor}
+        ${textColor}
+        ${className}`
+      }
+      {...props}
+    >
+      {children}
+    </button>
+  );
+}
+
+export default Button;
