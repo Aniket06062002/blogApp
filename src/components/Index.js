fc3562cf309aeee0aa56b3e5dcfd388c004522e0
Aniket Raut <rautaniket640@gmail.com>
2024-04-12 17:44:05 +0530

    all pages Created

--- src/components/Index.js
+++ src/components/Index.js
@@ -3,11 +3,22 @@ import Footer from "./Footer/Footer";
 import Container from "../container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import Signup  from "./Signup";
+import Login from './Login'
+import Post from './PostCard'
+import PostForm from './Post-from/PostForm'
+import PostCard from "./PostCard";
 
 export{
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    Signup,
+    Login,
+    Post,
+    PostForm,
+    PostCard
+
 }
---
+++ src/pages/Login.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { login as LoginComponents } from '../components/Index'
+function Login() {
+    return (
+        <div className='py-8'><LoginComponents /></div>
+    )
+}
+
+export default Login
